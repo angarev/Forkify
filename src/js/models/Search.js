c534497c05ed8fdeda3ec932395523eea0e9518e
preslyp <pancho.angarev@gmail.com>
2020-03-25 23:07:44 +0200

    Add Search Model and View

---
+++ src/js/models/Search.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+class Search {
+	constructor(query) {
+		this.query = query;
+	}
+
+	/**
+	 * Get search data from API response
+	 */
+	async getResults() {
+		try {
+			const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+			this.result = res.data.recipes;
+		} catch (error) {
+			alert(error);
+		}
+	}
+}
+
+export default Search;
