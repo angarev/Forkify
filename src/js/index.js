96a69082b4d642a6ca86ebada2f45a67c4264f45
preslyp <pancho.angarev@gmail.com>
2020-03-30 20:45:49 +0300

    Updating Recipe Servings

--- src/js/index.js
+++ src/js/index.js
@@ -100,3 +100,20 @@ const controlRecipe = async () => {
 	}
 };
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+//Handling recipe button clicks
+
+elements.recipe.addEventListener('click', e => {
+	console.log(e.target);
+
+	if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+		if (state.recipe.servings > 1) {
+			state.recipe.updateServings('dec');
+			recipeView.updateServingsIngredients(state.recipe);
+		}
+	}
+	if (e.target.matches('.btn-increase, .btn-increase *')) {
+		state.recipe.updateServings('inc');
+		recipeView.updateServingsIngredients(state.recipe);
+	}
+});
